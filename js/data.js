f63806254eb12197307ba619cce722bb78d7dd38
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-02-15 17:02:33 +0100

    changed function name init into loadUsers

--- js/data.js
+++ js/data.js
@@ -4,11 +4,9 @@ let users = [{
     'password': 'test1'
 }];
 
-let guest = [];
-
 
 // Function is added to body with onload. It fetches the user data from the backend server
-async function init() {
+async function laodUsers() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
 }
