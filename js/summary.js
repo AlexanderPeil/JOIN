1cbc143e786f50a25219eb433bb6f97505c58bd6
Wladimir Erlenbusch <wladimir_erlenbusch@hotmail.de>
2023-02-27 19:36:12 +0100

    updated UrgentCount and Deadline Date on summary.html/summary.js

--- js/summary.js
+++ js/summary.js
@@ -2,7 +2,6 @@ async function initSummary() {
     await includeHTML();
     await loadUsers();
     await loadAllTasks();
-    showDlDate();
     showGreet();
     loadNotes();
 }
@@ -20,6 +19,8 @@ function showAllCounts() {
     showCountAwaitFeedback();
     showCountToDo();
     showCountDone();
+    showCountUrgent();
+    showDlDate();
 }
 
 function showCountInBoard() {
@@ -49,6 +50,17 @@ function showCountAwaitFeedback() {
     return countAwaitFeedback.innerHTML = count;
 }
 
+function showCountUrgent() {
+    let countUrgent = document.getElementById('countUrgent');
+    let count = 0;
+    for (let i = 0; i < tasksTest.length; i++) {
+        if (tasksTest[i].priotity[0].priotity === "urgent") {
+            count++;
+        }
+    }
+    return countUrgent.innerHTML = count;
+}
+
 function showCountToDo() {
     let countToDo = document.getElementById('countToDo');
     let count = 0;
@@ -72,11 +84,25 @@ function showCountDone() {
 }
 /* End of: Show counts of board on summary*/
 
+/*
 function showDlDate() {
     const date = new Date();
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const dateString = date.toLocaleDateString('en-US', options);
     document.getElementById('dlDate').innerHTML = dateString;
+}*/
+
+function showDlDate() {
+    let dlDate = tasksTest[0].date;
+    for (let i = 1; i < tasksTest.length; i++) {
+        if (new Date(tasksTest[i].date) < new Date(dlDate)) {
+            dlDate = tasksTest[i].date;
+        }
+    }
+    const date = new Date(dlDate);
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    const formattedDate = date.toLocaleDateString("en-US", options);
+    return document.getElementById('dlDate').innerHTML = formattedDate;
 }
 
 function showGreet() {
