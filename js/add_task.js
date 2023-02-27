8ab81c7b60f9b9fe071c7c459c5d0c6579edd43a
Neesou <93050748+Neesou@users.noreply.github.com>
2023-02-27 16:04:16 +0100

    Add Add-Task Logic and change some logic at board

--- js/add_task.js
+++ js/add_task.js
@@ -1,5 +1,73 @@
-function init() {
+// Test-JSON for Add-Task
+let tasksTest = [
+    {
+        'split': 'to_do',
+        'category': 'Design',
+        'body_header': 'Website redesign',
+        'body_content': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eveniet reiciendis ea eaque veritatis ipsam repellat explicabo ab, ex aliquam accusamus consequatur, neque sit. Impedit delectus sequi rem quisquam eaque!',
+        'progress': [1, 2],
+        'users': ["SM", "PD", "EF"],
+        'priotity': "assets/img/low_priotity.png",
+        'date':"2023-02-28",
+        'subtasks':''
+    },
+    {
+        'split': 'in_progress',
+        'category': 'Sales',
+        'body_header': 'Website redesign',
+        'body_content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'progress': [],
+        'users': ["SM", "MV", "EF"],
+        'priotity': 'assets/img/medium_priotity.png',
+        'date':'2023-02-26',
+        'subtasks':''
+    },
+    {
+        'split': 'awaiting_feedback',
+        'category': 'Backoffice',
+        'body_header': 'Website redesign',
+        'body_content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'progress': [],
+        'users': ["SM", "EF"],
+        'priotity': 'assets/img/low_priotity.png',
+        'date':'2023-03-28',
+        'subtasks':''
+    },
+    {
+        'split': 'awaiting_feedback',
+        'category': 'Media',
+        'body_header': 'Website redesign',
+        'body_content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'progress': [],
+        'users': ["SM", "PD", "EF"],
+        'priotity': 'assets/img/medium_priotity.png',
+        'date':'2023-02-28',
+        'subtasks':''
+    },
+    {
+        'split': 'done',
+        'category': 'Marketing',
+        'body_header': 'Website redesign',
+        'body_content': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'progress': [3, 3],
+        'users': ["SM", "MV", "EF"],
+        'priotity': 'assets/img/high_priotity.png',
+        'date':'2023-02-23',
+        'subtasks':''
+    }
+];
+
+currentCategory = '';
+let contacts = loadContacts();
+
+let priotity_urgent = false;
+let priotity_medium = false;
+let priotity_low = true;
+
+async function init() {
     includeHTML();
+    loadNotes();
+    addAssignedToList();
 }
 
 /*********Change button color of prio**********/
@@ -93,4 +161,156 @@ function showLowWhiteBtn() {
         lowBtnWhite.classList.remove('d-none');
         lowBtnGreen.classList.add('d-none');
     }
+}
+
+//Fill the JSON (at Moment Test-JSON Local)
+
+async function addTask() {
+    let title = document.getElementById('title_textfield').value;
+    let description = document.getElementById('description_textfield').value;
+    let category = currentCategory;
+    let assigned_to = [];
+    let due_date = document.getElementById('date').value;
+    let new_task;
+
+    for (let i = 0; i < contacts.length; i++) {
+        if(document.getElementById('assigned-to-'+i).checked){
+            user = document.getElementById('assigned-to-'+i).value;
+            console.log(user);
+            assigned_to.push(user);
+        }
+        
+    }
+
+    new_task = {
+        'split': 'to_do',
+        'category': category,
+        'body_header': title,
+        'body_content': description,
+        'progress': '',
+        'users': assigned_to,
+        'priotity': checkPrioity(),
+        'date':due_date,
+        'subtasks':''
+    }
+
+    tasksTest.push(new_task);
+    await saveNotes();
+
+    //window.location.href='../board.html'
+}
+
+function changeColor() {
+    priotity_urgent = document.getElementById('urgentBtn').checked;
+    priotity_medium = document.getElementById('mediumBtn').checked;
+    priotity_low = document.getElementById('lowBtn').checked;
+
+    if (priotity_urgent) {
+        document.getElementById('urgentSection').innerHTML = `
+        Urgent<img
+        src="assets/img/Prio-urgent-white.png"></label>
+        `;
+        document.getElementById('mediumSection').innerHTML = `
+        Medium<img
+        src="assets/img/Prio-medium.png"></label>
+        `;
+        document.getElementById('lowSection').innerHTML = `
+        Low<img
+        src="assets/img/Prio-low.png"></label>
+        `;
+    }
+    if (priotity_medium) {
+        document.getElementById('urgentSection').innerHTML = `
+        Urgent<img
+        src="assets/img/Prio-urgent.png"></label>
+        `;
+        document.getElementById('mediumSection').innerHTML = `
+        Medium<img
+        src="assets/img/prio-medium-white.png"></label>
+        `;
+        document.getElementById('lowSection').innerHTML = `
+        Low<img
+        src="assets/img/Prio-low.png"></label>
+        `;
+
+    }
+    if (priotity_low) {
+        document.getElementById('urgentSection').innerHTML = `
+        Urgent<img
+        src="assets/img/Prio-urgent.png"></label>
+        `;
+        document.getElementById('mediumSection').innerHTML = `
+        Medium<img
+        src="assets/img/Prio-medium.png"></label>
+        `;
+        document.getElementById('lowSection').innerHTML = `
+        Low<img
+        src="assets/img/Prio-low-white.png"></label>
+        `;
+
+    }
+
+    console.log('Hoch: ' + priotity_urgent + ' Mittel: ' + priotity_medium + ' Niedrig: ' + priotity_low);
+}
+
+function checkPrioity(){
+    let prio;
+
+    if(priotity_low){
+        prio = "assets/img/low_priotity.png";
+    }
+    else if(priotity_medium){
+        prio ="assets/img/medium_priotity.png";
+    }
+    else if(priotity_urgent){
+        prio = "assets/img/high_priotity.png";
+    }
+
+    return prio;
+}
+
+async function saveNotes() {
+    let tasksAsJson = JSON.stringify(tasksTest);
+    await backend.setItem('allTasks', tasksAsJson);
+}
+
+async function loadNotes() {
+    await downloadFromServer();
+    tasksTest = JSON.parse(backend.getItem('allTasks')) || [];
+}
+
+async function loadContacts(){
+    let response = await fetch('./js/contact.json');
+    contacts = await response.json();
+}
+
+function addAssignedToList(){
+    document.getElementById('assigned-to-choices').innerHTML = '';
+    // document.getElementById('assigned-to-choices').innerHTML += `<select id=assigned-test></select>`;
+    
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        let firstName = contact['firstName'];
+        let lastName = contact['lastName'];
+        let acronym = firstName[0]+lastName[0];
+
+        console.log('Vorname: ' + firstName + ' | Nachname: ' + lastName + ' | Abkürzung: ' + acronym);
+
+        document.getElementById('assigned-to-choices').innerHTML += `<div class="assigned-to-line"><label for="assigned-to-${i}">${firstName + ' ' + lastName }</label><input type="checkbox" id="assigned-to-${i}" value="${acronym}"></div>`
+    }
+}
+
+
+function openDropdown(id){
+    if(document.getElementById(id).classList.contains('d-none')) {
+    document.getElementById(id).classList.remove('d-none');
+}
+    else if(!document.getElementById(id).classList.contains('d-none')) {
+        document.getElementById(id).classList.add('d-none');}
+}
+
+
+function changeCategoryHeader(name){
+    document.getElementById('category-header').innerHTML = name;
+    currentCategory = name;
 }
--- js/board.js
+++ js/board.js
@@ -1,55 +1,11 @@
-let tasks = [
-    {
-        "split": "to_do",
-        "category": "Design",
-        "body_header": "Website redesign",
-        "body_content": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eveniet reiciendis ea eaque veritatis ipsam repellat explicabo ab, ex aliquam accusamus consequatur, neque sit. Impedit delectus sequi rem quisquam eaque!",
-        "progress": [1, 2],
-        "users": ["SM", "PD", "EF"],
-        "priotity": "assets/img/low_priotity.png"
-    },
-    {
-        "split": "in_progress",
-        "category": "Sales",
-        "body_header": "Website redesign",
-        "body_content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-        "progress": [],
-        "users": ["SM", "MV", "EF"],
-        "priotity": "assets/img/medium_priotity.png"
-    },
-    {
-        "split": "awaiting_feedback",
-        "category": "Backoffice",
-        "body_header": "Website redesign",
-        "body_content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-        "progress": [],
-        "users": ["SM", "EF"],
-        "priotity": "assets/img/low_priotity.png"
-    },
-    {
-        "split": "awaiting_feedback",
-        "category": "Media",
-        "body_header": "Website redesign",
-        "body_content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-        "progress": [],
-        "users": ["SM", "MV", "LG"],
-        "priotity": "assets/img/medium_priotity.png"
-    },
-    {
-        "split": "done",
-        "category": "Marketing",
-        "body_header": "Website redesign",
-        "body_content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-        "progress": [3, 3],
-        "users": ["SM", "MV", "EF"],
-        "priotity": "assets/img/high_priotity.png"
-    }
-]
+let tasks = [];
+let users_color = loadContacts();
 
 let currentDraggedElement;
 
-function init() {
+async function init() {
     includeHTML();
+    await loadNotes()
     loadBoard();
 }
 function loadBoard() {
@@ -58,6 +14,8 @@ function loadBoard() {
     loadNewBoard();
     addDropArea();
 }
+
+
 
 
 function cleanOldBoard() {
@@ -73,8 +31,6 @@ function loadNewBoard() {
 
     for (let i = 0; i < tasks.length; i++) {
         const task = tasks[i];
-        console.log(task);
-        console.log(task['users'][2]);
         let catgoryLow = task['category'].toLowerCase();
 
         document.getElementById(task['split']).innerHTML += `
@@ -110,7 +66,7 @@ function loadNewBoard() {
         for (let j = 0; j < task['users'].length; j++) {
             const user = task['users'][j];
 
-            document.getElementById('users' + i).innerHTML += `<p style="background-color: var(--${user});">${user}</p>`
+            document.getElementById('users' + i).innerHTML += `<p style="background-color: blue;">${user}</p>`
         }
 
     }
@@ -131,8 +87,8 @@ function addDropArea() {
 
 function startDragging(id) {
     currentDraggedElement = id;
-    document.getElementById('card'+id).classList.add('d-none');
-    document.getElementById('card'+id).style.transform = "rotate(10deg)";
+    //document.getElementById('card'+id).classList.add('d-none');
+    //document.getElementById('card'+id).style.transform = "rotate(10deg)";
 }
 
 function allowDrop(ev, test) {
@@ -145,11 +101,27 @@ function diableDrop(ev) {
     document.getElementById('dropArea_'+ev).classList.remove('borders');
 }
 
-function moveTo(category) {
+async function moveTo(category) {
     tasks[currentDraggedElement]['split'] = category;
-    loadBoard()
+    loadBoard();
+    await saveNotes();
 }
 
 function endDragging(id) {
     document.getElementById('card'+id).style.transform = "rotate(0deg)";
+}
+
+async function loadNotes() {
+    await downloadFromServer();
+    tasks = JSON.parse(backend.getItem('allTasks')) || [];
+}
+
+async function saveNotes() {
+    let tasksAsJson = JSON.stringify(tasks);
+    await backend.setItem('allTasks', tasksAsJson);
+}
+
+async function loadContacts(){
+    let response = await fetch('./js/contact.json');
+    users_color = await response.json();
 }
