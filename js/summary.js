9240bd6e938a6c4c45d0a7f1748ae6ee84b4e341
Wladimir Erlenbusch <wladimir_erlenbusch@hotmail.de>
2023-03-10 19:30:17 +0100

    updated showGreetMobile function

--- js/summary.js
+++ js/summary.js
@@ -24,12 +24,14 @@ function showAllCounts() {
 }
 
 function showGreetMobile() {
-    let greetMobile = document.getElementById("greetMobile");
-    if (document.referrer.includes("index.html")) {
-        greetMobile.classList.remove('d-none');
-        setTimeout(function() {
-            greetMobile.classList.add('d-none');
-        }, 3000);
+    if (window.innerWidth < 1000) {
+        let greetMobile = document.getElementById("greetMobile");
+        if (document.referrer.includes("index.html")) {
+            greetMobile.classList.remove('d-none');
+            setTimeout(function() {
+                greetMobile.classList.add('d-none');
+            }, 3000);
+        }
     }
 }
 
